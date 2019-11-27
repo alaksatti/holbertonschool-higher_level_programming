2ed24950a39ac49dd3c8c085f156ef6d50ab159b
Aalaa Satti <ala.k.satti@gmail.com>
2019-11-27 01:51:05 +0000

    initial commits

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+--  lists all genres of the show Dexter.
+  FROM tv_genres
+LEFT JOIN tv_show_genres
+       ON genre_id = tv_genres.id
+LEFT JOIN tv_shows
+       ON show_id = tv_shows.id
+    WHERE title = "Dexter"
+ ORDER BY name ASC;
