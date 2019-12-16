5d834d3e14e1acc0eb6d218105d54de11044bb52
alaksatti <ala.k.satti@gmail.com>
2019-12-16 08:54:02 +0000

    added order

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name='{} ORDER BY states.id'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
