0af7b3db3e688c4d3328665c968592c21eb7c7df
alaksatti <ala.k.satti@gmail.com>
2019-12-16 13:03:53 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,11 +17,9 @@ if __name__ == "__main__":
     state = session.query(State).first()
 
     if state:
-      print("{}: {}".format(state.id, state.name))
-
+        print("{}: {}".format(state.id, state.name))
 
     else:
         print("Nothing")
 
     session.close()
-
