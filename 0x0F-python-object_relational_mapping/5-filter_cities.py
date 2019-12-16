b91c0e08fbaff7aebb7bcf8f7473bedfb0dfcdf0
alaksatti <ala.k.satti@gmail.com>
2019-12-16 12:10:23 +0000

    remove AS

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,7 +2,6 @@
 '''
 lists cities from a state taken as an argument"
 '''
-
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -13,8 +12,8 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities AS a JOIN states AS states ON "
-                "cities.state_id = states.id WHERE states.name = %s ORDER BY states.id",
+    cur.execute("SELECT cities.name FROM cities JOIN states on "
+                "cities.state_id = states.id WHERE states.name = %s",
                 (argv[4], ))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
