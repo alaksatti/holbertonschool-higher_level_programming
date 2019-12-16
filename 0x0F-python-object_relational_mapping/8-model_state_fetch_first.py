2a9363dbbc8ef6ae810981170f03b8dc852554e5
alaksatti <ala.k.satti@gmail.com>
2019-12-16 12:58:27 +0000

    rearrange if else

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,15 +10,18 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-        format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
     session = sessionmaker(bind=engine)()
     lists = session.query(State).first()
 
-    if lists is None:
-        print("Nothing")
-    else:
+    if lists:
       print("{}: {}".format(state.id, state.name))
+
+
+    else:
+        print("Nothing")
+
     session.close()
 
