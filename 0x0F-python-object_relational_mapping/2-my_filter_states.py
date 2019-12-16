e6f469aedb2c5ea45ffe643f0aa60f6e3a563fb2
alaksatti <ala.k.satti@gmail.com>
2019-12-16 08:39:56 +0000

    add binary search

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states"))
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
