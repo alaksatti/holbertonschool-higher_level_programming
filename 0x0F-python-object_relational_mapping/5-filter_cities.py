7b285fe3708253f41246d3fd5433aceeae436fef
alaksatti <ala.k.satti@gmail.com>
2019-12-16 09:15:41 +0000

    order by cities.id

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities"
                 "AS a JOIN states AS states ON cities.state_id = states.id"
-                "WHERE states.name = %s", (argv[4], ))
+                "WHERE states.name = %s ORDER BY cities.id", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
