b2f4fb44b9cbb92ccbca0eb8c40da1f4472ce7b8
alaksatti <ala.k.satti@gmail.com>
2019-12-16 08:50:44 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+lists states that match arguments from database
+'''
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    db.close()
