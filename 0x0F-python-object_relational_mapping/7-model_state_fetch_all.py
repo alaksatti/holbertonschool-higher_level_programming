e9e8651f4636ff1fb4693410f1b96d7fbd7a2a11
alaksatti <ala.k.satti@gmail.com>
2019-12-16 12:53:53 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,16 +9,13 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
 
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-        format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
-
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
     session = sessionmaker(bind=engine)()
     lists = session.query(State).all()
 
     for state in lists:
-      print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
-
