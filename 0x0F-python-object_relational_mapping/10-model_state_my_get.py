eb8f925e42b1678339d035c6743d168a9211c4e4
alaksatti <ala.k.satti@gmail.com>
2019-12-16 13:27:27 +0000

    pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-prints the State object with the name passed as argument from the database hbtn_0e_6_usa
+prints the State object w. name as arg from the database hbtn_0e_6_usa
 '''
 
 if __name__ == "__main__":
