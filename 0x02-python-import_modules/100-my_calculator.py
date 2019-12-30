a7dafdbed3e2ebd27f23b00eeed71b9ba333edd2
alaksatti <ala.k.satti@gmail.com>
2019-12-30 04:46:24 +0000

    pep8 compliant

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        
     elif argv[2] not in ['+', '-', '/', '*']:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
