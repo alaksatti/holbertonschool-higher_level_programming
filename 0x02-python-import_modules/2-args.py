59e30779f2d5ea094f9ea312ae288bdcd6aac01a
alaksatti <ala.k.satti@gmail.com>
2019-12-30 03:05:47 +0000

    account for 0 arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    if len(argv) != 2:
+    if len(argv) not in [1, 2]:
         print((str(len(argv) - 1)) + " arguments:")
+    elif len(argv) == 1:
+        print("0 arguments.")
     else:
         print("1 argument:")
     for i in range(1, len(argv)):
