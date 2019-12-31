1b157bb1e294cec04da4978b3ecfb0e4dfce3769
alaksatti <ala.k.satti@gmail.com>
2019-12-31 20:15:37 +0000

    pep8 compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,7 @@ if number < 0:
     LD = -1 * (n_number % 10)
 else:
     LD = number % 10
+    
 print("Last digit of " + str(number) + " is " + str(LD), end=" ")
 if LD > 5:
     print("and is greater than 5")
@@ -13,4 +14,3 @@ elif LD is 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-
