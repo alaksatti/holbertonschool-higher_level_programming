d4431c6f62aa75bf73741b2c13aab74161822beb
alaksatti <ala.k.satti@gmail.com>
2019-12-31 22:25:54 +0000

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 for i in reversed(range(ord('a'), ord('z') + 1)):
-    if (i % 2 == 0):
-        set = 0
+    if i % 2 != 0:
+        print(chr(i - 32), end='')
     else:
-        set = -32
-
-    print("{:s}".format(chr(i + set)), end='')
+        print(chr(i), end='')
