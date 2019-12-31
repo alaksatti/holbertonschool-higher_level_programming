a3c9cffd629af47276612a3671d0cf21800f126b
alaksatti <ala.k.satti@gmail.com>
2019-12-31 21:22:51 +0000

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(0, 100):
-    j = i // 10
-    k = i % 10
-
-    if j < k and i != 89:
-        print("{:.0f}{:.0f}, ".format((i // 10), (i % 10)), end='')
-    if i == 89:
-        print("89")
+for i in range(89):
+    if (i // 10 < i % 10):
+        print("{:02d}".format(i), end=", ")
+print('89')
