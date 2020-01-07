ab9a93460751924bc28e545a8a1f997b35809494
alaksatti <ala.k.satti@gmail.com>
2020-01-07 00:55:36 +0000

    pep8 compliant

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,19 +2,18 @@
 def find_peak(list_of_integers):
     """ find peak in unsorted integers"""
     l = list_of_integers
-    
+
     if l is None or len(l) == 0:
         return None
 
     if l[0] > l[1]:
         return l[0]
-    
+
     if l[-1] > l[-2]:
         return l[-1]
-    
+
     for x in range(1, (len(l) - 1)):
         if l[x] > l[x - 1]:
             if l[x] > l[x + 1]:
                 return l[x]
     return l[1]
-
