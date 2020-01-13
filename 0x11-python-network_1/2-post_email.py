d51a694c73676702721f57c0f863585383df8fd4
alaksatti <ala.k.satti@gmail.com>
2020-01-13 21:10:20 +0000

    add gaurd

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,7 @@ from urllib.request import urlopen
 from urllib.parse import urlencode
 from sys import argv
 
-data = urlencode({'email': argv[2]}).encode('utf-8')
-with urlopen(argv[1], data) as fetched:
-    print(fetched.read().decode('utf-8'))
+if __name__ == "__main__":
+    data = urlencode({'email': argv[2]}).encode('utf-8')
+    with urlopen(argv[1], data) as fetched:
+        print(fetched.read().decode('utf-8'))
