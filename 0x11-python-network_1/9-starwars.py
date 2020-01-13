dd0e77eb4ab347ece2fa168e599d19a6349e52e8
alaksatti <ala.k.satti@gmail.com>
2020-01-13 22:26:08 +0000

    initial commit

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a string and sends a search request to the Star Wars API
+"""
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    r = post('https://swapi.co/api/people', params={'search': argv[1]})
+    if r.json():
+        print("Number of results: {}".format(r.json()['count']))
+        for user in r.json()['results']:
+            print(user['name'])
+    else:
+        print('Not a valid JSON')
+            
