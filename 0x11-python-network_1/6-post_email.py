853058e024a0df6fa751dab175307fea392f6966
alaksatti <ala.k.satti@gmail.com>
2020-01-13 21:34:51 +0000

    initial commit

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3                                                              
+""" takes in a URL and an email, sends a POST request to the passed URL with th\
+e email as a parameter, and displays the body of the response (decoded in utf-8\
+) """
+
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    fetched = post(argv[1], data={'email': argv[2]})
+    print(fetched.text)
