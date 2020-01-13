fe959be3e29d02cfffbcea11bd4104d059b4e5cd
alaksatti <ala.k.satti@gmail.com>
2020-01-13 21:25:10 +0000

    fix print statements

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,5 @@ from requests import get
 
 r = get("https://intranet.hbtn.io/status")
 print("Body response:")
-print("\t- type: {}".format(type(r)))
-print("\t- content: {}".format(r))
-print("\t- utf8 content: {}".format(r.decode('utf-8')))
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.content.decode('utf-8')))
