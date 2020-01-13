31cb242e981ea57bbc5628a7e67f4727eb0d4059
alaksatti <ala.k.satti@gmail.com>
2020-01-13 22:47:33 +0000

    initial commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Takes 2 arguments in order to ist 10 commits
+(from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the Github API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    c = get('https://api.github.com/repos/' + argv[2] + '/' + argv[1] + '/commits')
+    if c.json():
+        c_num = 0
+        for commits in c.json():
+            if c_num < 10:
+                commit = commits.get('sha')
+                name = commits.get('commit').get('author').get('name')
+                print(commit + ': ' + name)
+                c_num += 1
+
+    else:
+        print("Not a valid JSON")
+                
