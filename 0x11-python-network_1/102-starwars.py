a652996a399140519b00ac2f2b17a0ef73d8df6e
alaksatti <ala.k.satti@gmail.com>
2020-01-14 01:57:33 +0000

    fix

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -3,7 +3,6 @@
 Write a Python script that takes in a string and
 sends a search request to the Star Wars API
 """
-from requests import post
 from requests import get
 from sys import argv
 
@@ -11,17 +10,15 @@ from sys import argv
 if __name__ == "__main__":
     r = get('https://swapi.co/api/people', params={'search': argv[1]})
 
-    if r.json():
-        results = r.json().get('results')
+    res = r.json().get('results')
 
-        print("Number of results: {}".format(r.json().get('count')))
-        tr = r.json()
-        while(tr.get('next')):
-            tr = get(tr.get('next')).json()
-            results += tr.get('results')
-        for e in results:
-            print(e.get('name'))
-            for film in e.get('films'):
-                print("\t{}".format(get(film).json().get('title')))
-    else:
-        print("Not a valid JSON")        
+    print("Number of results: {}".format(r.json().get('count')))
+
+    tr = r.json()
+    while(tr.get('next')):
+        tr = get(tr.get('next')).json()
+        res += tr.get('results')
+    for e in res:
+        print(e.get('name'))
+        for film in e.get('films'):
+            print("\t{}".format(get(film).json().get('title')))
