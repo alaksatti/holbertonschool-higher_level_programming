7ee33ee8f13b752cde90b7ff17a2d620f85b0fe7
alaksatti <ala.k.satti@gmail.com>
2020-02-04 23:08:17 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script prints 3 lines
+let arg = parseInt(process.argv.length);
+if (arg === 2) {
+  console.log('No argument');
+} else if (arg === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
