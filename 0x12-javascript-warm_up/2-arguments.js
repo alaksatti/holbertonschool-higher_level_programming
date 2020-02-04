6fe7df1c9326db80d35349d3746c1bdb00687fc2
alaksatti <ala.k.satti@gmail.com>
2020-02-04 23:15:20 +0000

    const instead of let

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script prints 3 lines
-let arg = parseInt(process.argv.length);
-if (arg === 2) {
+const length = parseInt(process.argv.length);
+if (length === 2) {
   console.log('No argument');
-} else if (arg === 3) {
+} else if (length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
