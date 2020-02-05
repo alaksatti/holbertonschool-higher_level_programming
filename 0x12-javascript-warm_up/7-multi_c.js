f8431d53d08fca2445ccd04aa803bd30f815f9b9
alaksatti <ala.k.satti@gmail.com>
2020-02-05 00:08:33 +0000

    estupido

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 // script that prints 3 lines
 let num = process.argv[2];
 if (num) {
-  for (let num = 0; num < x; num++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 } else {
