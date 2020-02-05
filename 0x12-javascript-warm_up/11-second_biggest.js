588b216bbaf6ee98ca0f738cd6f795b666d1fbb7
alaksatti <ala.k.satti@gmail.com>
2020-02-05 06:37:31 +0000

    semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // search the second biggest integer
 let arr = [];
-let n = process.argv.length;
+const n = process.argv.length;
 if (n < 4) {
   console.log(0);
 } else {
-  for (let i = 0; i < n ; i++) {
+  for (let i = 0; i < n; i++) {
     if (parseInt(process.argv[i])) {
       arr.push(parseInt(process.argv[i]));
     }
