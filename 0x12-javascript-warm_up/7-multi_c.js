44abd1bdfdf3fa022b19c27d82e4a8cf9dcefbec
alaksatti <ala.k.satti@gmail.com>
2020-02-05 00:07:19 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints 3 lines
+let num = process.argv[2];
+if (num) {
+  for (let num = 0; num < x; num++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
