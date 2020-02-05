6cdd781b649aae255bb9f176194f20f0a0d2f77b
alaksatti <ala.k.satti@gmail.com>
2020-02-05 00:16:46 +0000

    semistandard fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // print a square
-const side = parseInt(process.argv[2])
+const side = parseInt(process.argv[2]);
 if (side) {
   for (let i = 0; i < side; i++) {
-      console.log('X'.repeat(side));
-    }
+    console.log('X'.repeat(side));
+  }
 } else {
-  console.log('Missing size')
+  console.log('Missing size');
 }
