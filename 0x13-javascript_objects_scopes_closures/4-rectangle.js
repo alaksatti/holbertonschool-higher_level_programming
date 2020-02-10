32fc0a99c416892b1e36fe0bed8bfffe192c4ac1
alaksatti <ala.k.satti@gmail.com>
2020-02-10 23:57:11 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// create an empty class rectangle '
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) { return this; }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
