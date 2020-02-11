19ef346fce87a4a4ac27582d55e628b33c886197
alaksatti <ala.k.satti@gmail.com>
2020-02-11 01:18:28 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// returns the number of occurences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let i = 0; i < list.length; i++) { if (list[i] === searchElement) { count += 1; } }
+
+  return count;
+};
