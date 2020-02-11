e5235aa18b55524f3814acba2c8065cb4bebc053
alaksatti <ala.k.satti@gmail.com>
2020-02-11 07:20:41 +0000

    remove new lines

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,4 +2,4 @@
 // concats 2 files
 const fs = require('fs');
 
-fs.writeFileSync(process.argv[4], fs.readFileSync(process.argv[2]) + '\n' + fs.readFileSync(process.argv[3]) + '\n');
+fs.writeFileSync(process.argv[4], fs.readFileSync(process.argv[2]) + fs.readFileSync(process.argv[3]));
