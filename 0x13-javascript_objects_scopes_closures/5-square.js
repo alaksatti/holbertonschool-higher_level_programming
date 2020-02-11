59f83f154a0b3971b534a1bdef768b68d4533d8b
alaksatti <ala.k.satti@gmail.com>
2020-02-11 00:24:05 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// class square
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle { constructor (size) { super(size, size); }}
+module.exports = Square;
