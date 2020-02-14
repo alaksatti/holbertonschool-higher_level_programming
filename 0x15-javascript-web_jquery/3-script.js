135dafbd2c96188ec13f3c4e6860e803e4f5d0b7
alaksatti <ala.k.satti@gmail.com>
2020-02-14 02:49:56 +0000

    change the header to red

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,4 +1,4 @@
 // change header to red when user clicks, cannot use document.querySelector
-$('#red_header').click(function () {
-  $('#red_header').addClass('red');
+$('div#red_header').click(function () {
+  $('header').addClass('red');
 });
