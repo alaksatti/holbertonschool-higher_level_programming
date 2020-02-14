4ca6ea21561641a9bebf841e5aa002d66f99948f
alaksatti <ala.k.satti@gmail.com>
2020-02-14 08:37:53 +0000

    initial commit

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,14 @@
+// adds, removes and clears LI elements from a list when the user clicks.
+$(function () {
+  $('#add_item').click(function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+
+  $('#remove_item').click(function () {
+    $('.my_list li').last().remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('.my_list').empty();
+  });
+});
